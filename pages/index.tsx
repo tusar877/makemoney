46272dd3829d3cfd685b87c16024c638c18bf950
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-10-25 21:54:13 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.location.href = 'https://newsing.xyz/';
+    window.location.href = 'https://warriorplus.com/o2/a/y50bpr/0';
   }, []);
 
   return null;
